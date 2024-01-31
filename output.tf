8f7cc75dcda1461829aa25f72d1b660cd47a25ef
nidakhan99 <153353850+nidakhan99@users.noreply.github.com>
2024-01-31 19:59:16 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,1 +1,20 @@
 
+output "vpcid" {
+  description = "ID of project VPC"
+  value       = aws_vpc.VPC1.id
+}
+
+output "Igw" {
+  description = "ID of subnets"
+  value       = aws_internet_gateway.gw.id
+  }
+
+
+output "pub-sub-ids" {
+  value = [for subnet in aws_subnet.pubsubnets : subnet.id]
+}
+
+output "pir-sub-ids" {
+  value = [for subnet in aws_subnet.pirsubnets : subnet.id]
+}
+
